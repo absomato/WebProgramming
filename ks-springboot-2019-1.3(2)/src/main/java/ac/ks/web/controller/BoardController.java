e5f1a7b81e074d008bd2195328a732d73ff4f82b
absomato <absomato@naver.com>
2019-05-16 16:06:27 +0900

    finish init

--- ks-springboot-2019-1.3(2)/src/main/java/ac/ks/web/controller/BoardController.java
+++ ks-springboot-2019-1.3(2)/src/main/java/ac/ks/web/controller/BoardController.java
@@ -1,13 +1,54 @@
 package ac.ks.web.controller;
 
+import ac.ks.web.domain.Board;
+import ac.ks.web.domain.BoardType;
+import ac.ks.web.repository.BoardRepository;
+import ac.ks.web.service.BoardService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class BoardController {
+    private BoardRepository boardRepository;
+    private BoardService boardService;
 
-    @GetMapping("/helloworld")
-    public String helloWorld() {
-        return "helloworld";
+    public BoardController(BoardRepository boardRepository, BoardService boardService) {
+        this.boardRepository = boardRepository;
+        this.boardService = boardService;
     }
+
+    @GetMapping("/")
+    public String list(@PageableDefault Pageable pageable, Model model) {
+        model.addAttribute("boardList", boardService.findBoardList(pageable));
+        return "index";
+    }
+
+    @GetMapping("/new")
+    public String form(Board board){
+        return "new";
+    }
+
+    @PostMapping("/add")
+    public String add(Board board, Model model) {
+        board.setBoardType(BoardType.free);
+        board.setCreatedDate(LocalDateTime.now());
+        board.setUpdatedDate(LocalDateTime.now());
+        Board saveBoard = boardRepository.save(board);
+        model.addAttribute("board", boardService.findBoardByIdx(saveBoard.getIdx()));
+        return "item";
+    }
+
+    @GetMapping("/{idx}")
+    public String read(@PathVariable Long idx, Model model) {
+        model.addAttribute("board", boardService.findBoardByIdx(idx));
+        return "item";
+    }
+
 }
